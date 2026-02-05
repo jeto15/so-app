fa48021a80fc78ced22ec52ff7fc805670dea17e
jeto15 <jetpersonal2@gmail.com>
2026-02-05 19:50:10 +0800

    Update login.ts

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -33,12 +33,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         res.status(200).json({ token });
     } catch (error) {
-        console.error("LOGIN ERROR:", err); // shows real error in PM2 logs
+        console.error("LOGIN ERROR:", error); // shows real error in PM2 logs
 
             res.status(500).json({
                 message: "Server error",
-                error: err.message,
-                code: err.code
+                error: error.message,
+                code: error.code
             });
     }
 }
