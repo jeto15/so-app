3b174026bd6ee38a63cc4b3b13ed25da98d0a943
jeto15 <jetpersonal2@gmail.com>
2026-02-05 19:45:54 +0800

    Log username and password on login attempt

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
 
     const { username, password } = req.body;
-
+    console.log(username,password);
     try {
         const [rows] = await db.query<RowDataPacket[]>("SELECT * FROM users WHERE username = ?", [username]);
 
